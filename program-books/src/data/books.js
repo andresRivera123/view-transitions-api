7353c4b37acbc11bb38e77a5b7f5beb05a06ef1b
Andyk <carlosandresriveravalle@gmail.com>
2025-01-26 20:31:23 +0000

    Create [Book] independent pages

---
+++ program-books/src/data/books.js
@@ -0,0 +1,34 @@
+export const books = [
+  {
+    id: "dont-make-me-think",
+    title: "Don't Make Me Think: Sentido común para la usabilidad web",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1711564661i/18197267.jpg",
+    author: "Steve Krug",
+    opinion:
+      "En esta tercera edición, Steve regresa con una perspectiva fresca para reexaminar los principios que hicieron de Don't Make Me Think un clásico, con ejemplos actualizados y un nuevo capítulo sobre la usabilidad móvil. Y sigue siendo breve, profusamente ilustrado... y lo mejor de todo: divertido de leer.",
+  },
+  {
+    id: "clean-code",
+    title: "Clean code: Manual de desarrollo ágil de software",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+    author: "Robert C. Martín",
+    opinion:
+      "Incluso un código malo puede funcionar, pero si no está limpio, puede hacer que una organización de desarrollo se hunda. Cada año, se pierden incontables horas y recursos importantes debido a un código mal escrito, pero no tiene por qué ser así. El reconocido experto en software Robert C. Martin presenta un paradigma revolucionario con Clean Code.",
+  },
+  {
+    id: "lean-ux",
+    title:
+      "Lean UX: Aplicación de los principios Lean para mejorar la experiencia del usuario",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1381355830i/13436116.jpg",
+    author: "Jeff Gothelf, Josh Seiden",
+    opinion:
+      "El enfoque Lean UX para el diseño de interacción está hecho a medida para la realidad actual impulsada por la web. En este revelador libro, el destacado defensor Jeff Gothelf le enseña principios, tácticas y técnicas valiosas de Lean UX desde cero.",
+  },
+  {
+    id: "eloquent-javascript",
+    title: "Una introducción moderna a la programación",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+    author: "Marijn Haverbeke",
+    opinion:"Una mezcla concisa y equilibrada de principios y pragmática. Me encantó el desarrollo de programas con estilo tutorial y estilo de juego. Este libro reavivó mis primeros placeres de la programación. ¡Y además, JavaScript!"
+  },
+];
